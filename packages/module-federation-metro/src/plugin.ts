f8999d8a98f2f387b4ee71767303623ad0997568
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-04-14 13:44:50 +0200

    feat: add basic MF config object type

--- packages/module-federation-metro/src/plugin.ts
+++ packages/module-federation-metro/src/plugin.ts
@@ -1,6 +1,24 @@
-import type { MetroConfig } from "metro-config";
+import path from "node:path";
+import type { ConfigT } from "metro-config";
 
-function withModuleFederation(config: MetroConfig) {
+interface ModuleFederationConfiguration {
+  name: string;
+  filename: string;
+  shared: Record<
+    string,
+    {
+      singleton: boolean;
+      eager: boolean;
+      version: string;
+      requiredVersion: string;
+    }
+  >;
+}
+
+function withModuleFederation(
+  config: ConfigT,
+  options: ModuleFederationConfiguration
+): ConfigT {
   console.log("withModuleFederation plugin");
 
   return config;
