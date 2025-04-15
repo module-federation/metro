c5f1309f9cea1841e637ed914d53cb09032056d3
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-04-15 11:04:58 +0200

    chore: add mfr, fix setup

--- apps/host/metro.config.js
+++ apps/host/metro.config.js
@@ -9,10 +9,12 @@ const {withModuleFederation} = require('module-federation-metro');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
+
 const config = {
   watchFolders: [
     path.resolve(__dirname, '../../node_modules'),
     path.resolve(__dirname, '../../external/metro/packages'),
+    path.resolve(__dirname, '../../packages/module-federation-metro'),
   ],
 };
 
--- packages/module-federation-metro/index.js
+++ packages/module-federation-metro/index.js
@@ -2,6 +2,7 @@ try {
   const path = require("path");
   require("ts-node").register({
     project: path.resolve(__dirname, "tsconfig.json"),
+    transpileOnly: true,
   });
 } catch {}
 
