ca0a0a59c235d7ee0d99cd22b21de90cd50fd4fc
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-04-15 12:42:40 +0200

    refactor: remove eval once boundary

--- packages/module-federation-metro/src/runtime/init-host.js
+++ packages/module-federation-metro/src/runtime/init-host.js
@@ -1,19 +1,15 @@
 import { init } from "@module-federation/runtime";
 
-let __once__ = false;
 const usedShared = __SHARED__;
 
-if (!__once__) {
-  __once__ = true;
-  const initRes = init({
-    name: __NAME__,
-    shared: usedShared,
-    shareStrategy: "loaded-first",
-  });
+const initRes = init({
+  name: __NAME__,
+  shared: usedShared,
+  shareStrategy: "loaded-first",
+});
 
-  initRes.initializeSharing("default", {
-    strategy: "loaded-first",
-    from: "build",
-    initScope: [],
-  });
-}
+initRes.initializeSharing("default", {
+  strategy: "loaded-first",
+  from: "build",
+  initScope: [],
+});
