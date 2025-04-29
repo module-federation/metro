30cc1fb78c30c2f23da0b83e505209a15fd3b992
Kacper Wiszczuk <kacperwiszczuk@gmail.com>
2025-04-29 16:23:31 +0200

    chore: refactor generation functions

--- packages/module-federation-metro/src/generate-manifest.ts
+++ packages/module-federation-metro/src/generate-manifest.ts
@@ -1,4 +1,4 @@
-import type { Manifest } from "@module-federation/sdk";
+import type { Manifest, StatsAssets } from "@module-federation/sdk";
 import type { ModuleFederationConfiguration } from "./plugin";
 
 export default function generateManifest(
@@ -7,69 +7,92 @@ export default function generateManifest(
   return {
     id: config.name,
     name: config.name,
-    metaData: {
-      name: config.name,
-      type: "app",
-      buildInfo: {
-        buildVersion: "1.0.0",
-        buildName: config.name,
-      },
-      remoteEntry: {
-        name: `${config.filename}.bundle`,
-        path: "",
-        type: "global",
-      },
-      types: {
-        path: "",
-        name: "",
-        api: "",
-        zip: "",
-      },
-      globalName: config.name,
-      pluginVersion: "",
-      publicPath: "auto",
+    metaData: generateMetaData(config),
+    remotes: generateRemotes(config),
+    shared: generateShared(config),
+    exposes: generateExposes(config),
+  };
+}
+
+function generateMetaData(
+  config: ModuleFederationConfiguration
+): Manifest["metaData"] {
+  return {
+    name: config.name,
+    type: "app",
+    buildInfo: {
+      buildVersion: "1.0.0",
+      buildName: config.name,
     },
-    remotes: Object.keys(config.remotes || {}).map((remote) => ({
-      federationContainerName: config.remotes[remote],
-      moduleName: remote,
-      alias: remote,
-      entry: "*",
-    })),
-    shared: Object.keys(config.shared).map((shared) => ({
-      id: shared,
-      name: shared,
-      version: config.shared[shared].version,
-      requiredVersion: config.shared[shared].requiredVersion,
-      singleton: config.shared[shared].singleton,
-      hash: "",
-      assets: {
-        js: {
-          sync: [],
-          async: [],
-        },
-        css: {
-          sync: [],
-          async: [],
-        },
-      },
-    })),
-    exposes: Object.keys(config.exposes || {}).map((expose) => {
-      const formatKey = expose.replace("./", "");
-      return {
-        id: `${config.name}:${formatKey}`,
-        name: formatKey,
-        path: expose,
-        assets: {
-          js: {
-            sync: [(config.exposes || {})[expose]],
-            async: [],
-          },
-          css: {
-            sync: [],
-            async: [],
-          },
-        },
-      };
-    }),
+    remoteEntry: {
+      name: `${config.filename}.bundle`,
+      path: "",
+      type: "global",
+    },
+    types: {
+      path: "",
+      name: "",
+      api: "",
+      zip: "",
+    },
+    globalName: config.name,
+    pluginVersion: "",
+    publicPath: "auto",
+  };
+}
+
+function generateRemotes(
+  config: ModuleFederationConfiguration
+): Manifest["remotes"] {
+  return Object.keys(config.remotes).map((remote) => ({
+    federationContainerName: config.remotes[remote],
+    moduleName: remote,
+    alias: remote,
+    entry: "*",
+  }));
+}
+
+function generateShared(
+  config: ModuleFederationConfiguration
+): Manifest["shared"] {
+  return Object.keys(config.shared).map((shared) => ({
+    id: shared,
+    name: shared,
+    version: config.shared[shared].version,
+    requiredVersion: config.shared[shared].requiredVersion,
+    singleton: config.shared[shared].singleton,
+    hash: "",
+    assets: getEmptyAssets(),
+  }));
+}
+
+function generateExposes(
+  config: ModuleFederationConfiguration
+): Manifest["exposes"] {
+  return Object.keys(config.exposes).map((expose) => {
+    const formatKey = expose.replace("./", "");
+    const assets = getEmptyAssets();
+
+    assets.js.sync.push(config.exposes[expose]);
+
+    return {
+      id: `${config.name}:${formatKey}`,
+      name: formatKey,
+      path: expose,
+      assets,
+    };
+  });
+}
+
+function getEmptyAssets(): StatsAssets {
+  return {
+    js: {
+      sync: [],
+      async: [],
+    },
+    css: {
+      sync: [],
+      async: [],
+    },
   };
 }
