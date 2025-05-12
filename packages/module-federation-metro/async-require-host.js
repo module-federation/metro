ac858c29cc1b90b5357e843021bc308ab8c0a0e1
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-05-12 14:29:20 +0200

    fix: dont modify fetchAsync

--- packages/module-federation-metro/async-require-host.js
+++ packages/module-federation-metro/async-require-host.js
@@ -1,1 +1,5 @@
+// fetchAsync.native.ts requires process.env.EXPO_OS to be set
+// since expo is optional, we set it to an empty string as a fallback
+process.env.EXPO_OS = process.env.EXPO_OS ?? "";
+
 require("./vendor/expo/async-require");
