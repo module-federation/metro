1aaf2ed57ed3ac9e732b42b6890bb878fc51955e
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-05-16 14:36:54 +0200

    feat: rework loadShareAsync

--- packages/module-federation-metro/src/runtime/shared-registry.js
+++ packages/module-federation-metro/src/runtime/shared-registry.js
@@ -3,27 +3,20 @@ import { loadShare, loadShareSync } from "@module-federation/runtime";
 const registry = {};
 const loading = {};
 
-export function loadSharedToRegistry(id) {
-  if (id === "react" || id === "react-native") {
-    return loadSharedToRegistrySync(id);
-  } else {
-    return loadSharedToRegistryAsync(id);
-  }
-}
-
 export async function loadSharedToRegistryAsync(id) {
-  await global.__METRO_FEDERATION__[__NAME__].__shareInit;
   const promise = loading[id];
   if (promise) {
     await promise;
   } else {
     registry[id] = {};
-    loading[id] = loadShare(id);
-
-    const shared = (await loading[id])();
-    Object.getOwnPropertyNames(shared).forEach((key) => {
-      registry[id][key] = shared[key];
-    });
+    loading[id] = (async () => {
+      const factory = await loadShare(id);
+      const sharedModule = factory();
+      Object.getOwnPropertyNames(sharedModule).forEach((key) => {
+        const descriptor = Object.getOwnPropertyDescriptor(sharedModule, key);
+        Object.defineProperty(registry[id], key, descriptor);
+      });
+    })();
   }
 }
 
