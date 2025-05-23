2cef0cb0c11caa09f5dd042b53ba546280fbf8a0
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-05-23 15:16:14 +0200

    feat: keep the wrapper for prod

--- packages/module-federation-metro/async-require-remote.js
+++ packages/module-federation-metro/async-require-remote.js
@@ -3,4 +3,31 @@ if (!process.env.EXPO_OS) {
   require("./vendor/expo/async-require");
 }
 
-// TODO prod
+if (process.env.NODE_ENV === "production") {
+  function loadBundleAsyncMFWrapper(bundlePath) {
+    function joinComponents(prefix, suffix) {
+      return prefix.replace(/\/+$/, "") + "/" + suffix.replace(/^\/+/, "");
+    }
+
+    function getPublicPath(url) {
+      return url.split("/").slice(0, -1).join("/");
+    }
+
+    const loadBundleAsync =
+      global[`${__METRO_GLOBAL_PREFIX__ ?? ""}__loadBundleAsync`];
+
+    const remoteEntry =
+      global.__METRO_FEDERATION__[__METRO_GLOBAL_PREFIX__].location;
+
+    // resolve the remote bundle path based on the remote location
+    const remoteBundlePath = joinComponents(
+      getPublicPath(remoteEntry),
+      bundlePath
+    );
+
+    return loadBundleAsync(remoteBundlePath);
+  }
+
+  global[`${__METRO_GLOBAL_PREFIX__}__loadBundleAsync`] =
+    loadBundleAsyncMFWrapper;
+}
