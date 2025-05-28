4d35210bd9a9c4421374941fffa270530eebca5b
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-05-28 14:40:24 +0200

    feat: replace middleware with rewriteRequestUrl

--- packages/module-federation-metro/src/plugin.ts
+++ packages/module-federation-metro/src/plugin.ts
@@ -483,16 +483,18 @@ function withModuleFederation(
     },
     server: {
       ...config.server,
-      enhanceMiddleware: createEnhanceMiddleware(
-        MANIFEST_FILENAME,
-        manifestPath
-      ),
       rewriteRequestUrl(url) {
         const { pathname } = new URL(url, "protocol://host");
         // rewrite /mini.bundle -> /mini.js.bundle
         if (pathname.startsWith(`/${options.filename}`)) {
           const target = replaceExtension(options.filename, ".js.bundle");
           return url.replace(options.filename, target);
+        }
+        // rewrite /mf-manifest.json -> /[metro-project]/node_modules/.mf-metro/mf-manifest.json
+        if (pathname.startsWith(`/${MANIFEST_FILENAME}`)) {
+          const root = config.projectRoot;
+          const target = manifestPath.replace(root, "[metro-project]");
+          return url.replace(MANIFEST_FILENAME, target);
         }
         // pass through to original rewriteRequestUrl
         if (config.server.rewriteRequestUrl) {
