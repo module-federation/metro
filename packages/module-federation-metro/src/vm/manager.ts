47f82a8fb8f975f5a5c6d999af00f097541770f2
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-06-06 17:05:15 +0200

    feat: vm manager

---
+++ packages/module-federation-metro/src/vm/manager.ts
@@ -0,0 +1,118 @@
+import connect from "connect";
+import type MetroServer from "metro/src/Server";
+import type { FileSystem } from "metro-file-map";
+import { GetTransformOptions, MetroConfig } from "metro-config";
+
+type Bundler = ReturnType<ReturnType<MetroServer["getBundler"]>["getBundler"]>;
+
+const isRadonIDE = "REACT_NATIVE_IDE_LIB_PATH" in process.env;
+
+export class VirtualModuleManager {
+  private enabled: Promise<boolean> | false = false;
+  private virtualModules: Map<string, string> = new Map();
+
+  constructor(
+    private metroConfig: MetroConfig,
+    private forceWriteFileSystem: boolean
+  ) {}
+
+  getMiddleware() {
+    return (middleware: any, metroServer: MetroServer) => {
+      const server = connect();
+      const bundler = metroServer.getBundler().getBundler();
+
+      if (!this.forceWriteFileSystem && !isRadonIDE) {
+        this.setup(bundler);
+
+        server.use(async (_, __, next) => {
+          // Wait until the bundler patching has completed
+          await this.enabled;
+          next();
+        });
+      }
+
+      const originalMiddleware = this.metroConfig.server?.enhanceMiddleware;
+      return originalMiddleware
+        ? server.use(originalMiddleware(middleware, metroServer))
+        : server.use(middleware);
+    };
+  }
+
+  getTransformOptions(): GetTransformOptions {
+    return async (entryPoints, transformOptions, getDependenciesOf) => {
+      if (this.enabled) {
+        await this.enabled;
+      }
+
+      // We need to write to the file system if virtual modules are not possible and/or we are building for production
+      // const writeToFileSystem = !this.enabled || !transformOptions.dev;
+
+      const originalGetTransformOptions =
+        this.metroConfig.transformer?.getTransformOptions;
+
+      return Object.assign(
+        {},
+        await originalGetTransformOptions?.(
+          entryPoints,
+          transformOptions,
+          getDependenciesOf
+        )
+      );
+    };
+  }
+
+  private setup(bundler: Bundler) {
+    this.enabled = (async () => {
+      const graph = await bundler.getDependencyGraph();
+      // @ts-expect-error incomplete types
+      this.ensureFileSystemPatched(graph._fileSystem);
+      this.ensureBundlerPatched(bundler);
+      return true;
+    })();
+  }
+
+  // Patch the bundler to use virtual modules
+  private ensureFileSystemPatched(
+    fs: FileSystem & { getSha1: { __vm__patched?: boolean } }
+  ) {
+    if (!fs.getSha1.__vm__patched) {
+      const original_getSha1 = fs.getSha1.bind(fs);
+      fs.getSha1 = (filename) => {
+        if (this.virtualModules.has(filename)) {
+          // Don't cache this file. It should always be fresh.
+          return `${filename}-${Date.now()}`;
+        }
+        return original_getSha1(filename);
+      };
+      fs.getSha1.__vm__patched = true;
+    }
+
+    return fs;
+  }
+
+  // Patch the bundler to use virtual modules
+  private ensureBundlerPatched(
+    bundler: Bundler & { transformFile: { __vm__patched?: boolean } }
+  ) {
+    if (bundler.transformFile.__vm__patched) {
+      return;
+    }
+
+    const manager = this;
+    const transformFile = bundler.transformFile.bind(bundler);
+
+    bundler.transformFile = async function (
+      filePath,
+      transformOptions,
+      fileBuffer
+    ) {
+      const virtualModule = manager.virtualModules.get(filePath);
+
+      if (virtualModule) {
+        fileBuffer = Buffer.from(virtualModule);
+      }
+      return transformFile(filePath, transformOptions, fileBuffer);
+    };
+    bundler.transformFile.__vm__patched = true;
+  }
+}
