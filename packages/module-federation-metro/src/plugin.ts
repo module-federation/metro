b014adcb3cd61649bede24279e16db0bb3c4e2b9
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-06-09 20:09:53 +0200

    feat: disable MF for non-mf commands

--- packages/module-federation-metro/src/plugin.ts
+++ packages/module-federation-metro/src/plugin.ts
@@ -1,5 +1,6 @@
 import path from "node:path";
 import fs from "node:fs";
+import chalk from "chalk";
 import type { ConfigT } from "metro-config";
 import type { Resolution } from "metro-resolver";
 import generateManifest from "./generate-manifest";
@@ -255,6 +256,14 @@ function replaceExtension(filepath: string, extension: string) {
   return path.format({ dir, name, ext: extension });
 }
 
+function isUsingMFCommands() {
+  const command = process.argv[2];
+  if (command === "bundle-mf-host" || command === "bundle-mf-remote") {
+    return true;
+  }
+  return false;
+}
+
 function validateOptions(options: ModuleFederationConfigNormalized) {
   // validate filename
   if (!options.filename.endsWith(".bundle")) {
@@ -298,6 +307,22 @@ function withModuleFederation(
   config: ConfigT,
   federationOptions: ModuleFederationConfig
 ): ConfigT {
+  if (!isUsingMFCommands()) {
+    console.warn(
+      chalk.yellow(
+        "Warning: Module Federation build is disabled for this command.\n"
+      ) +
+        chalk.yellow(
+          "To enable Module Federation, please use one of the dedicated bundle commands:\n"
+        ) +
+        ` ${chalk.dim("•")} bundle-mf-host` +
+        chalk.dim(" - for bundling a host application\n") +
+        ` ${chalk.dim("•")} bundle-mf-remote` +
+        chalk.dim(" - for bundling a remote application\n")
+    );
+    return config;
+  }
+
   const isHost = !federationOptions.exposes;
   const isRemote = !isHost;
 
