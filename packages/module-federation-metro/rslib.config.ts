c1092d1b873e87b2a0e748c71db60d1f3e55287f
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-06-16 22:37:44 +0200

    chore: include cjs

--- packages/module-federation-metro/rslib.config.ts
+++ packages/module-federation-metro/rslib.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from '@rslib/core';
 export default defineConfig({
   lib: [
     { format: 'esm', syntax: 'es2021', bundle: false, dts: { bundle: false } },
+    { format: 'cjs', syntax: 'es2021', bundle: false },
   ],
   source: {
     entry: {
