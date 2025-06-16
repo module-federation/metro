063c2f32d7a4fdbe2935b07eb120b997c851f4fd
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-06-16 19:52:00 +0200

    feat: esm only

--- packages/module-federation-metro/rslib.config.ts
+++ packages/module-federation-metro/rslib.config.ts
@@ -2,10 +2,18 @@ import { defineConfig } from '@rslib/core';
 
 export default defineConfig({
   lib: [
-    { format: 'cjs', syntax: 'es2021', bundle: false, dts: { bundle: false } },
-    { format: 'esm', syntax: 'es2021', bundle: false },
+    { format: 'esm', syntax: 'es2021', bundle: false, dts: { bundle: false } },
   ],
   source: {
+    entry: {
+      index: 'src/!(modules|runtime)',
+    },
     tsconfigPath: './tsconfig.build.json',
+  },
+  output: {
+    copy: [
+      { from: './src/modules', to: 'modules' },
+      { from: './src/runtime', to: 'runtime' },
+    ],
   },
 });
