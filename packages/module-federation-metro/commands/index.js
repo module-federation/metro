44cdb5cdb246722408a1aaeaa40d8bc84342a328
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-06-16 22:16:17 +0200

    feat: custom dev condition

--- packages/module-federation-metro/commands/index.js
+++ packages/module-federation-metro/commands/index.js
@@ -1,13 +1,1 @@
-try {
-  const path = require('node:path');
-  require('ts-node').register({
-    project: path.resolve(__dirname, '../tsconfig.json'),
-    transpileOnly: true,
-    compilerOptions: {
-      module: 'commonjs',
-      moduleResolution: 'node',
-    },
-  });
-} catch {}
-
-module.exports = require('../src/commands/index');
+module.exports = require('../dist/commands/index.js');
