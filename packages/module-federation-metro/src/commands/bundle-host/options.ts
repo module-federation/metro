60353835093cc73f76fb2c368b816a4aa6a8d0b2
Kacper Wiszczuk <kacperwiszczuk@gmail.com>
2025-06-17 15:33:54 +0200

    fix: move hack to plugin

--- packages/module-federation-metro/src/commands/bundle-host/options.ts
+++ packages/module-federation-metro/src/commands/bundle-host/options.ts
@@ -3,13 +3,4 @@ import { getCommunityCliPlugin } from "../utils/getCommunityPlugin";
 const communityCliPlugin = getCommunityCliPlugin();
 const options = communityCliPlugin.bundleCommand.options;
 
-const extendedOptions = [
-  ...options,
-  {
-    name: "--config-cmd [string]",
-    description:
-      "[Internal] A hack for Xcode build script pointing to wrong bundle command that recognizes this flag. Do not use.",
-  },
-];
-
-export default extendedOptions;
+export default options;
