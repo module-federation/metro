a90820e89118bd16899024a3835e035dbfd72136
Kacper Wiszczuk <kacperwiszczuk@gmail.com>
2025-06-17 11:12:39 +0200

    fix: e2e

--- packages/module-federation-metro/src/commands/bundle-host/options.ts
+++ packages/module-federation-metro/src/commands/bundle-host/options.ts
@@ -3,4 +3,13 @@ import { getCommunityCliPlugin } from "../utils/getCommunityPlugin";
 const communityCliPlugin = getCommunityCliPlugin();
 const options = communityCliPlugin.bundleCommand.options;
 
-export default options;
+const extendedOptions = [
+  ...options,
+  {
+    name: "--config-cmd [string]",
+    description:
+      "[Internal] A hack for Xcode build script pointing to wrong bundle command that recognizes this flag. Do not use.",
+  },
+];
+
+export default extendedOptions;
