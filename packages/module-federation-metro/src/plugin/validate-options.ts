328b897db3a5dd401cc3e083e79a70c3080a6fb9
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-06-23 15:39:37 +0200

    feat: allow only module names as shared

--- packages/module-federation-metro/src/plugin/validate-options.ts
+++ packages/module-federation-metro/src/plugin/validate-options.ts
@@ -18,6 +18,30 @@ function validateShared(shared: Shared) {
   if (Array.isArray(shared)) {
     throw new ConfigError('Array format is not supported for shared.');
   }
+
+  // validate shared module names
+  for (const sharedName of Object.keys(shared)) {
+    // disallow relative paths
+    if (sharedName.startsWith('./') || sharedName.startsWith('../')) {
+      throw new ConfigError(
+        'Relative paths are not supported as shared module names.'
+      );
+    }
+
+    // disallow absolute paths
+    if (sharedName.startsWith('/')) {
+      throw new ConfigError(
+        'Absolute paths are not supported as shared module names.'
+      );
+    }
+
+    // disallow deep import wildcards (containing /)
+    if (sharedName.endsWith('/')) {
+      throw new ConfigError(
+        'Deep import wildcards are not supported as shared module names.'
+      );
+    }
+  }
 }
 
 export function validateOptions(options: ModuleFederationConfigNormalized) {
