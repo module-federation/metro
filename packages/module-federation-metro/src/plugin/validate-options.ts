e69c944a3ef7c97ddb94552c5d8a0e54a65ebd50
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-06-23 15:32:39 +0200

    feat: validate shared is object

--- packages/module-federation-metro/src/plugin/validate-options.ts
+++ packages/module-federation-metro/src/plugin/validate-options.ts
@@ -1,12 +1,29 @@
-import type { ModuleFederationConfigNormalized } from '../types';
+import type { ModuleFederationConfigNormalized, Shared } from '../types';
 import { ConfigError } from '../utils';
 
-export function validateOptions(options: ModuleFederationConfigNormalized) {
-  // validate filename
-  if (!options.filename.endsWith('.bundle')) {
+function validateFilename(filename: string) {
+  if (!filename.endsWith('.bundle')) {
     throw new ConfigError(
-      `Invalid filename: ${options.filename}. ` +
+      `Invalid filename: ${filename}. ` +
         'Filename must end with .bundle extension.'
     );
   }
+}
+
+function validateShared(shared: Shared) {
+  if (!shared || typeof shared !== 'object') {
+    throw new ConfigError('Shared must be an object.');
+  }
+
+  if (Array.isArray(shared)) {
+    throw new ConfigError('Array format is not supported for shared.');
+  }
+}
+
+export function validateOptions(options: ModuleFederationConfigNormalized) {
+  // validate filename
+  validateFilename(options.filename);
+
+  // validate shared
+  validateShared(options.shared);
 }
