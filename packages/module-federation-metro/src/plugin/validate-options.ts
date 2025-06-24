03fa9f513f41e5f654d65710cf6f6fc47097ef5a
Jakub Romańczyk <jakub.romanczyk@callstack.com>
2025-06-24 10:17:06 +0200

    feat: validate shared (#67)

--- packages/module-federation-metro/src/plugin/validate-options.ts
+++ packages/module-federation-metro/src/plugin/validate-options.ts
@@ -1,12 +1,54 @@
-import type { ModuleFederationConfigNormalized } from '../types';
+import type { ModuleFederationConfigNormalized, Shared } from '../types';
 import { ConfigError } from '../utils';
 
-export function validateOptions(options: ModuleFederationConfigNormalized) {
-  // validate filename
-  if (!options.filename.endsWith('.bundle')) {
+function validateFilename(filename: string) {
+  if (!filename.endsWith('.bundle')) {
     throw new ConfigError(
-      `Invalid filename: ${options.filename}. ` +
+      `Invalid filename: ${filename}. ` +
         'Filename must end with .bundle extension.'
     );
   }
+}
+
+function validateShared(shared: Shared) {
+  if (!shared || typeof shared !== 'object') {
+    throw new ConfigError('Shared must be an object.');
+  }
+
+  if (Array.isArray(shared)) {
+    throw new ConfigError('Array format is not supported for shared.');
+  }
+
+  // validate shared module names
+  for (const sharedName of Object.keys(shared)) {
+    // disallow relative paths
+    if (sharedName.startsWith('./') || sharedName.startsWith('../')) {
+      throw new ConfigError(
+        'Relative paths are not supported as shared module names.'
+      );
+    }
+
+    // disallow absolute paths
+    if (sharedName.startsWith('/')) {
+      throw new ConfigError(
+        'Absolute paths are not supported as shared module names.'
+      );
+    }
+
+    // disallow deep import wildcards (containing /)
+    if (sharedName.endsWith('/')) {
+      throw new ConfigError(
+        'Deep import wildcards are not supported as shared module names. ' +
+          'You need to list all deep imports explicitly.'
+      );
+    }
+  }
+}
+
+export function validateOptions(options: ModuleFederationConfigNormalized) {
+  // validate filename
+  validateFilename(options.filename);
+
+  // validate shared
+  validateShared(options.shared);
 }
