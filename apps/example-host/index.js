43b645374556083e6d498514304f72126167824c
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-06-26 14:45:37 +0200

    chore: remove host workaround

--- apps/example-host/index.js
+++ apps/example-host/index.js
@@ -1,9 +1,3 @@
-// we need to explicitly import the init-host runtime module
-// this is because of a metro limitation, where the module
-// must be used in the bundle in order to be present in the final bundle
-import 'mf:init-host';
-import 'mf:async-require';
-
 import { withAsyncStartup } from '@module-federation/metro/bootstrap';
 import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
